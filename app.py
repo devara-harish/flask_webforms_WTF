68cc4315ddb3d641adc9a951e61954ab9a38c79d
devara-harish <devaraharish09@gmail.com>
2023-05-30 23:43:18 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask,render_template,request
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
+
+
+
+
+
+
+FAI=Flask(__name__)
+
+
+class NameForm(Form):
+    sname=StringField(validators=[DataRequired()])
+    submit=SubmitField()
+
+@FAI.route('/webform',methods=['GET','POST'])
+def webform():
+    NFO=NameForm()
+    if request.method=='POST':
+        NFD=NameForm(request.form)
+        if NFD.validate():
+            return NFD.sname.data
+
+    return render_template('webforms.html',NFO=NFO)
+
+
+
+if __name__=='__main__':
+
+    FAI.run(debug=True)
+
+
+
